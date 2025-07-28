3c9b829f89f66fc981082c31377433599f72753d
Quezty <storlijoachim@gmail.com>
2025-07-28 23:26:49 +0200

    Made basic placement of widgets

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from textual.app import App 
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, ListView, ListItem, Input, Label, Static
+from textual.containers import Horizontal
 
 class ProjectLauncher(App):
-    None 
-    
+
+    CSS_PATH = "styles.tcss"
+    TITLE = "Project Launcher"
+    SUB_TITLE = "A TUI project launcher and project manager"
+    THEME = "Tokyo Night"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Input()
+        yield Horizontal(
+            ListView(
+                ListItem(Label("One")),
+                ListItem(Label("Two")),
+                ListItem(Label("Three"))
+            ),
+            Static("test")
+        )
+        yield Footer()
 
 if __name__ == "__main__":
     app = ProjectLauncher()
