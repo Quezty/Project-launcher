1cba92ba7c4a8f8df62d637783e6d19b01360799
Joachim Storli <122435731+Quezty@users.noreply.github.com>
2025-07-29 21:19:44 +0000

    Added keybinds to move around the UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, ListView, ListItem, Input, Label, Static
 from textual.containers import Horizontal
+from textual.events import Key
 
 class ProjectLauncher(App):
 
@@ -9,18 +10,46 @@ class ProjectLauncher(App):
     SUB_TITLE = "A TUI project launcher and project manager"
     THEME = "Tokyo Night"
 
+    BINDINGS = [
+        ("q", "quit", "quit the app"),
+        ("s", "focus_searchbar", "search")
+    ]
+
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Input()
+        yield Input(id="Searchbar")
         yield Horizontal(
             ListView(
                 ListItem(Label("One")),
                 ListItem(Label("Two")),
-                ListItem(Label("Three"))
+                ListItem(Label("Three")),
+                id="ProjectList"
             ),
             Static("test")
         )
         yield Footer()
+    
+    def on_mount(self) -> None:
+        self.searchbar = self.query_one("#Searchbar", Input)
+        self.list = self.query_one("#ProjectList", ListView)
+        self.set_focus(self.list)
+
+    def on_key(self, event: Key) -> None:
+        if self.focused == self.list:
+            if event.key == "j":
+                self.list.index = min(self.list.index + 1, len(self.list.children) - 1)
+                event.stop()
+            elif event.key == "k":
+                self.list.index = max(self.list.index - 1, 0)
+                event.stop()
+        
+        if self.focused == self.searchbar:
+            if event.key == "escape":
+                self.set_focus(self.list)
+
+    def action_focus_searchbar(self) -> None:
+        self.set_focus(self.searchbar)
+
 
 if __name__ == "__main__":
     app = ProjectLauncher()
