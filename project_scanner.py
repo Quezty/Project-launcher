682ded89cbbcc839d6447e057b4b1639a402c320
Joachim Storli <122435731+Quezty@users.noreply.github.com>
2025-07-30 08:44:25 +0000

    Project_scanner.py first revision, projectfolders and readme

--- project_scanner.py
+++ project_scanner.py
@@ -1,8 +1,15 @@
 import os
 
-PROJECT_DIRECTORY = "~/repos"
+PROJECT_DIRECTORY = "/home/codespace/repos"
 
-with os.scandir("/") as it:
+with os.scandir(PROJECT_DIRECTORY) as it:
     for entry in it:
         if not entry.name.startswith('.') and entry.is_dir():
-            print(entry.name)
+            current_project_directory = PROJECT_DIRECTORY + "/" + entry.name
+            with os.scandir(current_project_directory) as list:
+                for file in list:
+                    if file.name == "readme.md" or file.name == "README.md":
+                        readme_file_path = current_project_directory + "/" + file.name
+                        with open(readme_file_path, "r", encoding="utf-8") as md_file:
+                            markdown_content = md_file.read()
+                        print(markdown_content) 
