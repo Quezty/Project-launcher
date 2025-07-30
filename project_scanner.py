9de8ccd33c49b807b788103710756e5b79e33057
Joachim Storli <122435731+Quezty@users.noreply.github.com>
2025-07-30 12:32:24 +0000

    Made some updates to project_scanner.py, ready for use now. Could use some prettyfying.

--- project_scanner.py
+++ project_scanner.py
@@ -1,15 +1,29 @@
 import os
+from dataclasses import dataclass
+
+@dataclass
+class DirectoryInfo:
+    name: str
+    path: str
+    markdown: str
 
 PROJECT_DIRECTORY = "/home/codespace/repos"
 
-with os.scandir(PROJECT_DIRECTORY) as it:
-    for entry in it:
-        if not entry.name.startswith('.') and entry.is_dir():
-            current_project_directory = PROJECT_DIRECTORY + "/" + entry.name
-            with os.scandir(current_project_directory) as list:
-                for file in list:
-                    if file.name == "readme.md" or file.name == "README.md":
-                        readme_file_path = current_project_directory + "/" + file.name
-                        with open(readme_file_path, "r", encoding="utf-8") as md_file:
-                            markdown_content = md_file.read()
-                        print(markdown_content) 
+def grab_project_info(directory):
+
+    objects = [] 
+
+    with os.scandir(directory) as root_dir:
+        for entry in root_dir:
+            if not entry.name.startswith('.') and entry.is_dir():
+                current_project_directory = directory + "/" + entry.name                
+                with os.scandir(current_project_directory) as subdir:
+                    for file in subdir:
+                        if file.name == "readme.md" or file.name == "README.md":
+                            readme_file_path = current_project_directory + "/" + file.name
+                            with open(readme_file_path, "r", encoding="utf-8") as md_file:
+                                markdown_content = md_file.read()
+                            
+                            project_info = DirectoryInfo(entry.name, current_project_directory, markdown_content)
+                            objects.append(project_info)
+    return objects
