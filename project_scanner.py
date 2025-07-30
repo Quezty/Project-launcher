8af1936f70c7ad43eaae2b85ca98208fcd370b0f
Quezty <storlijoachim@gmail.com>
2025-07-31 00:25:42 +0200

    Documented use of grab_project_info method

--- project_scanner.py
+++ project_scanner.py
@@ -9,21 +9,30 @@ class DirectoryInfo:
 
 PROJECT_DIRECTORY = "/home/codespace/repos"
 
-def grab_project_info(directory):
+def grab_project_info(directory: str) -> list[DirectoryInfo]:
+    """ Grabs readme information for all projects in given directory
 
-    objects = [] 
+    Args:
+        directory(str): root directory for projects
+
+    Returns:
+        list: Returns a list of DirectoryInfo objects, accessible through list_name[index].name/path/markdown
+
+    """
+
+    objects = []
 
     with os.scandir(directory) as root_dir:
         for entry in root_dir:
             if not entry.name.startswith('.') and entry.is_dir():
-                current_project_directory = directory + "/" + entry.name                
+                current_project_directory = directory + "/" + entry.name
                 with os.scandir(current_project_directory) as subdir:
                     for file in subdir:
-                        if file.name == "readme.md" or file.name == "README.md":
+                        if file.name in ("README.md", "readme.md"):
                             readme_file_path = current_project_directory + "/" + file.name
                             with open(readme_file_path, "r", encoding="utf-8") as md_file:
-                                markdown_content = md_file.read()
-                            
+                                markdown_content = md_file.read
+
                             project_info = DirectoryInfo(entry.name, current_project_directory, markdown_content)
                             objects.append(project_info)
     return objects
