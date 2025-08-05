344619f4063b3af90e25b3b0f75543a7a2397dbb
Quezty <storlijoachim@gmail.com>
2025-08-05 23:05:51 +0200

    Docstrings and formatting
    Added docstrings to methods for my nvim to stop complaining about
    missing docstrings, also did some deleting of whitespaces etc.

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, ListView, ListItem, Input, Label, Markdown
 from textual.containers import Horizontal
-from textual.events import Key
 from textual.reactive import reactive
 from textual.message import Message
 from project_scanner import grab_project_info
 
-PROJECT_ROOT_DIRECTORY = "/home/codespace/repos"
+PROJECT_ROOT_DIRECTORY = "/home/codespaces/repos"
 
 class ProjectSearchbar(Input):
+    """Input field with logic to unblur and clear itself"""
 
     BINDINGS = [
         ("ctrl+s", "blur_and_move_focus", "move to list"),
-        ("ctrl+d", "clear_search", "Clear searchbar")        
+        ("ctrl+d", "clear_search", "Clear searchbar")
     ]
 
     class BlurRequested(Message):
         def __init__(self, sender: "ProjectSearchbar") -> None:
             super().__init__()
             self.sender = sender
+            self.value = ""
 
     def action_blur_and_move_focus(self) -> None:
+        """Sends blur message to App class"""
         self.post_message(self.BlurRequested(self))
 
     def action_clear_search(self) -> None:
+        """Clears searchbar"""
         self.value = ""
 
 
 class ProjectLauncher(App):
+    """Main app class"""
 
     CSS_PATH = "styles.tcss"
     TITLE = "Project Launcher"
@@ -46,7 +50,7 @@ class ProjectLauncher(App):
     def __init__(self):
         super().__init__()
         self.PROJECTS = []
-        self.project_index = 0
+        self.filtered_projects = []
 
 
 
@@ -78,14 +82,17 @@ class ProjectLauncher(App):
         self.list.index = 0
         self.set_focus(self.list)
 
-    
+
     def action_move_down(self) -> None:
+        """Moves cursor 1 item down"""
         self.selected_index = self.selected_index + 1 if self.selected_index < len(self.list.children) - 1 else self.selected_index
 
     def action_move_up(self) -> None:
+        """Moves cursor 1 item up"""
         self.selected_index = self.selected_index - 1 if self.selected_index != 0  else self.selected_index
 
     def action_focus_searchbar(self) -> None:
+        """Focuses searchbar"""
         self.set_focus(self.searchbar)
 
     async def on_input_changed(self, event: Input.Changed) -> None:
@@ -95,22 +102,25 @@ class ProjectLauncher(App):
         filtered_projects = self.project_filter(self.PROJECTS, filter)
 
         for project in filtered_projects:
-            await self.list.append(ListItem(Label(project.name))) 
+            await self.list.append(ListItem(Label(project.name)))
 
         try:
             self.markdown.update(filtered_projects[0].markdown)
-        except:
-            self.markdown.update("No project selected/found")
-        
+        except IndexError:
+            self.markdown.update(f"No project found matching string {filter}!")
+
 
     def on_project_searchbar_blur_requested(self, event: ProjectSearchbar.BlurRequested) -> None:
         self.set_focus(self.list)
-        
-    
+
+
     def watch_selected_index(self, value: int) -> None:
-        self.list.index = value 
-        self.project_index = value
-        self.markdown.update(self.PROJECTS[value].markdown)
+        self.list.index = value
+        if not self.filtered_projects:
+            self.markdown.update(self.PROJECTS[value].markdown)
+        else:
+            self.markdown.update(self.filtered_projects[value].markdown)
+
 
     def project_filter(self, project_list: list, filter: str) -> list:
         filtered_list = []
